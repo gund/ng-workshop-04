d7db6d0ec30877512e6a47964c982c4d35c9c77e
gund <malkevich.alex@gmail.com>
2020-01-30 20:19:56 +0100

    test(app): add unit tests to people component

--- src/app/people/people.service.spec.ts
+++ src/app/people/people.service.spec.ts
@@ -1,16 +1,37 @@
-/* tslint:disable:no-unused-variable */
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
 
-import { TestBed, async, inject } from '@angular/core/testing';
 import { PeopleService } from './people.service';
 
 describe('Service: People', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [PeopleService]
+      // Usually mock Http services here...
+      providers: [PeopleService],
     });
   });
 
-  it('should ...', inject([PeopleService], (service: PeopleService) => {
-    expect(service).toBeTruthy();
-  }));
+  describe('getAll() method', () => {
+    it('should return observable of mocked data', fakeAsync(() => {
+      const callback = jasmine.createSpy();
+
+      getService()
+        .getAll()
+        .subscribe(callback);
+
+      // Simulate 5s time passage
+      tick(5000);
+
+      expect(callback).toHaveBeenCalledWith([
+        'Vasya',
+        'Petya',
+        'John',
+        'Bob',
+        '',
+      ]);
+    }));
+  });
 });
+
+function getService(): PeopleService {
+  return TestBed.get(PeopleService);
+}
