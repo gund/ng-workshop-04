a58734f3c35a53742e4f99190814c663c06f613d
gund <malkevich.alex@gmail.com>
2020-01-30 20:22:23 +0100

    test(birthday): add unit test skeleton for component

---
+++ src/app/birthday/birthday.component.spec.ts
@@ -0,0 +1,17 @@
+/* tslint:disable:no-unused-variable */
+import { TestBed } from '@angular/core/testing';
+
+import { BirthdayComponent } from './birthday.component';
+
+describe('BirthdayComponent', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [BirthdayComponent],
+    });
+  });
+
+  it('should ...', () => {
+    const fixture = TestBed.createComponent(BirthdayComponent);
+    expect(fixture).toBeTruthy();
+  });
+});
