1e27e763b570e60a476611229fbb19b7940e8fc2
Aleksey Belan <alexey.belan@spryker.com>
2020-02-04 13:53:24 +0200

    Remove scheme

--- src/app/birthday/birthday.service.spec.ts
+++ src/app/birthday/birthday.service.spec.ts
@@ -1,43 +1,43 @@
 /* tslint:disable:no-unused-variable */
 
 import {TestBed, fakeAsync, tick} from '@angular/core/testing';
-import { BirthdayService } from './birthday.service';
+import {BirthdayService} from './birthday.service';
 
 describe('Service: Birthday', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [BirthdayService]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [BirthdayService]
+        });
     });
-  });
 
-  describe('getBirthdayFor() method', () => {
-    it('should return new Date observable', fakeAsync(() => {
-      const callback = jasmine.createSpy();
+    describe('getBirthdayFor() method', () => {
+        it('should return new Date observable', fakeAsync(() => {
+            const callback = jasmine.createSpy();
 
-      getService()
-          .getBirthdayFor('Vasya')
-          .subscribe(callback);
+            getService()
+                .getBirthdayFor('Vasya')
+                .subscribe(callback);
 
-      tick(5000);
+            tick(5000);
 
-      expect(callback).toHaveBeenCalledWith(jasmine.any(Date));
-    }));
+            expect(callback).toHaveBeenCalledWith(jasmine.any(Date));
+        }));
 
-    it('should return error observable', fakeAsync(() => {
-      const callback = jasmine.createSpy();
-      const errorObs = () => {
-        getService()
-            .getBirthdayFor('')
-            .subscribe(callback);
+        it('should return error observable', fakeAsync(() => {
+            const callback = jasmine.createSpy();
+            const errorObs = () => {
+                getService()
+                    .getBirthdayFor('')
+                    .subscribe(callback);
 
-        tick(5000);
-      };
+                tick(5000);
+            };
 
-      expect(errorObs).toThrow('Unable to find birthday for empty name!');
-    }));
-  });
+            expect(errorObs).toThrow('Unable to find birthday for empty name!');
+        }));
+    });
 });
 
 function getService(): BirthdayService {
-  return TestBed.get(BirthdayService);
+    return TestBed.get(BirthdayService);
 }
