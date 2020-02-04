45c8dc64e9ed678a68a49bf423da971806d779c3
Aleksey Belan <alexey.belan@spryker.com>
2020-02-04 13:49:56 +0200

    Tests HW

--- src/app/birthday/birthday.component.spec.ts
+++ src/app/birthday/birthday.component.spec.ts
@@ -1,17 +1,94 @@
 /* tslint:disable:no-unused-variable */
-import { TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { BirthdayComponent } from './birthday.component';
+import {BirthdayComponent} from './birthday.component';
+import {Subject} from 'rxjs';
+import {BirthdayService} from './birthday.service';
+import {CUSTOM_ELEMENTS_SCHEMA, SimpleChange} from '@angular/core';
+import { By } from '@angular/platform-browser';
+
+class BirthdayServiceMock {
+    getAllSubject$ = new Subject();
+    getBirthdayFor = jasmine.createSpy().and.returnValue(this.getAllSubject$);
+}
 
 describe('BirthdayComponent', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [BirthdayComponent],
-    }).compileComponents();
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [BirthdayComponent],
+            providers: [{ provide: BirthdayService, useClass: BirthdayServiceMock }],
+            schemas: [CUSTOM_ELEMENTS_SCHEMA],
+        }).compileComponents();
+    });
 
-  it('should ...', () => {
-    const fixture = TestBed.createComponent(BirthdayComponent);
-    expect(fixture).toBeTruthy();
-  });
+    let fixture: ComponentFixture<BirthdayComponent>;
+    const mockedName = 'Vasya';
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(BirthdayComponent);
+        const componentInstance = fixture.componentInstance;
+        componentInstance.name = mockedName;
+
+        componentInstance.ngOnChanges({
+            name: new SimpleChange(null, componentInstance.name, true)
+        });
+    });
+
+    it('should call `BirthdayService.getBirthdayFor()` on change name property', () => {
+        fixture.detectChanges();
+
+        expect(getService().getBirthdayFor).toHaveBeenCalled();
+    });
+
+    it('should render loading text in `p` with loading text on change name property', () => {
+        fixture.detectChanges();
+        console.log(fixture.debugElement.query(By.css('p')));
+
+        const pElem = fixture.debugElement.query(By.css('p'));
+
+        expect(pElem.nativeElement.textContent).toMatch(
+            `Loading birthday data for ${mockedName}...`,
+        );
+    });
+
+    it('should render error in `p` with error text when `BirthdayService.getBirthdayFor()` fails', () => {
+        fixture.detectChanges();
+
+        getService().getAllSubject$.error('reason');
+
+        fixture.detectChanges();
+
+        const pElem = fixture.debugElement.query(By.css('p'));
+
+        expect(pElem.nativeElement.textContent).toMatch(
+            'Failed to load birthday data: reason',
+        );
+    });
+
+    describe('when birthday component loaded', () => {
+        const mockedDate = new Date();
+
+        beforeEach(() => {
+            fixture.detectChanges();
+            getService().getAllSubject$.next(mockedDate);
+            fixture.detectChanges();
+        });
+
+        it('should render `p` with name and person birthday', () => {
+            const pElem = fixture.debugElement.query(By.css('p'));
+            const transformedDate = mockedDate.toLocaleDateString('en-US', {
+                year: '2-digit',
+                month: 'numeric',
+                day: 'numeric'
+            });
+
+            expect(pElem.nativeElement.textContent).toMatch(
+                `${mockedName}'s birthday is on ${transformedDate}`,
+            );
+        });
+    });
 });
+
+function getService(): BirthdayServiceMock {
+    return TestBed.get(BirthdayService);
+}
--- src/app/birthday/birthday.service.spec.ts
+++ src/app/birthday/birthday.service.spec.ts
@@ -1,6 +1,6 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async, inject } from '@angular/core/testing';
+import {TestBed, fakeAsync, tick} from '@angular/core/testing';
 import { BirthdayService } from './birthday.service';
 
 describe('Service: Birthday', () => {
@@ -10,7 +10,34 @@ describe('Service: Birthday', () => {
     });
   });
 
-  it('should ...', inject([BirthdayService], (service: BirthdayService) => {
-    expect(service).toBeTruthy();
-  }));
+  describe('getBirthdayFor() method', () => {
+    it('should return new Date observable', fakeAsync(() => {
+      const callback = jasmine.createSpy();
+
+      getService()
+          .getBirthdayFor('Vasya')
+          .subscribe(callback);
+
+      tick(5000);
+
+      expect(callback).toHaveBeenCalledWith(jasmine.any(Date));
+    }));
+
+    it('should return error observable', fakeAsync(() => {
+      const callback = jasmine.createSpy();
+      const errorObs = () => {
+        getService()
+            .getBirthdayFor('')
+            .subscribe(callback);
+
+        tick(5000);
+      };
+
+      expect(errorObs).toThrow('Unable to find birthday for empty name!');
+    }));
+  });
 });
+
+function getService(): BirthdayService {
+  return TestBed.get(BirthdayService);
+}
