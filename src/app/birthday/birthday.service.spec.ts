12ee22b64a7e71dc1ead7d28c89bd3d85305cc40
Dmytro Abramov <dmytro.abramov@spryker.com>
2020-02-21 15:02:58 +0200

    Added tests for service

--- src/app/birthday/birthday.service.spec.ts
+++ src/app/birthday/birthday.service.spec.ts
@@ -1,6 +1,6 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async, inject } from '@angular/core/testing';
+import { TestBed, inject, tick, fakeAsync } from '@angular/core/testing';
 import { BirthdayService } from './birthday.service';
 
 describe('Service: Birthday', () => {
@@ -10,7 +10,38 @@ describe('Service: Birthday', () => {
     });
   });
 
-  it('should ...', inject([BirthdayService], (service: BirthdayService) => {
+  it('Service should exist', inject([BirthdayService], (service: BirthdayService) => {
     expect(service).toBeTruthy();
   }));
+
+  describe('getBirthdayFor behavior', () => {
+    it('Should return Date object', fakeAsync(() => {
+      let methodReturnedValue: Date;
+  
+      getService()
+        .getBirthdayFor('Dima')
+        .subscribe(event =>  methodReturnedValue = event);
+  
+      tick(5000); //is it needed?
+  
+      expect(checkIfDateReturned(methodReturnedValue)).toBeFalsy();
+    }));
+  
+    it('Should throw Error if name wasn`t provided or an empty string was provided', fakeAsync(() => {
+      const errorText: string = 'Unable to find birthday for empty name!';
+  
+      getService().getBirthdayFor('').subscribe(
+        date => fail(`Expected an error, not ${date}`),
+        error  => expect(error).toContain(errorText)
+      )
+    }));
+  })  
 });
+
+function getService(): BirthdayService {
+  return TestBed.get(BirthdayService);
+}
+
+function checkIfDateReturned(value: any): boolean {
+  return isNaN(value.getTime()); // shouldn't be NaN as Date.getTime() returns number
+}
